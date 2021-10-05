f1a7cd648ce39938bc836b3313e21ee1b3e940c2
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-10-04 17:36:15 -0700

    Added basic navbar and Home route

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-wrapper white">
+        <a href="#" className="brand-logo left">
+          Logo
+        </a>
+        <ul id="nav-mobile" className="right">
+          <li>
+            <a href="sass.html">Login</a>
+          </li>
+          <li>
+            <a href="badges.html">Signup</a>
+          </li>
+          <li>
+            <a href="collapsible.html">Profile</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
