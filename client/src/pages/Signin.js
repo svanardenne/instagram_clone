6289ec6c3162c9ea58d8b97bc2ae47c3a2adfb52
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-10-05 15:36:54 -0700

    Added styled components to Signin.js

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,13 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+const CardWrapper = styled.div`
+  margin-top: 30px;
+`;
+
+const AuthCard = styled.div`
+  padding: 20px;
+  text-align: center;
+  max-width: 400px;
+  margin: 10px auto;
+`;
+
+const InputField = styled.input`
+  &:focus {
+    border-bottom: 1px solid #000 !important;
+    box-shadow: 0 1px 0 0 #000 !important;
+  }
+`;
+
 const Signin = () => {
   return (
-    <div>
-      <div className="card">
+    <CardWrapper>
+      <AuthCard className="card">
         <h2>Instagram</h2>
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="password" />
-        <button className="btn waves-effect waves-light">Login</button>
-      </div>
-    </div>
+        <InputField type="text" placeholder="email" />
+        <InputField type="text" placeholder="password" />
+        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">
+          Login
+        </button>
+      </AuthCard>
+    </CardWrapper>
   );
 };
 
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Signup = () => {
   return <h1>Signup</h1>;
 };
