b9b3d3f56d525dec43175fd3ace53c3f779f3ed5
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-10-05 17:16:13 -0700

    Added styling to profile div

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,62 @@
 import React from "react";
+import styled from "styled-components";
+
+const ProfileWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin: 18px 0px;
+  border-bottom: 1px solid grey;
+  @media (max-width: 435px) {
+    h4 {
+      font-size: 20px;
+    }
+  }
+`;
+
+const ProfileImage = styled.img`
+  width: 160px;
+  height: 160px;
+  border-radius: 80px;
+`;
+
+const ProfileTitle = styled.div`
+  margin: 0 10px;
+`;
+
+const PostInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 15px;
+  h6 {
+    margin: 0 !important;
+  }
+  @media (max-width: 500px) {
+    flex-direction: column;
+    gap: 5;
+    h6 {
+      font-size: 14px;
+    }
+  }
+`;
 
 const Profile = () => {
-  return <h1>Profile</h1>;
+  return (
+    <div>
+      <ProfileWrapper>
+        <div>
+          <ProfileImage src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+        </div>
+        <ProfileTitle>
+          <h4>Jenna Harrington</h4>
+          <PostInfo>
+            <h6>40 posts</h6>
+            <h6>40 followers</h6>
+            <h6>40 following</h6>
+          </PostInfo>
+        </ProfileTitle>
+      </ProfileWrapper>
+    </div>
+  );
 };
 
 export default Profile;
