7b97ff71f4b281100bcc43d0933733291a233495
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-10-09 16:08:13 -0700

    Added all fields in CreatePost.js to state

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const CreatePostCard = styled.div`
@@ -9,14 +9,37 @@ const CreatePostCard = styled.div`
 `;
 
 const CreatePost = () => {
+  const [values, setValues] = useState({
+    title: "",
+    body: "",
+    image: "",
+  });
+
+  const { title, body } = values;
+
+  const handleChange = (fieldName) => (e) => {
+    const value = fieldName === "image" ? e.target.files[0] : e.target.value;
+    setValues({ ...values, [fieldName]: value });
+  };
+
   return (
     <CreatePostCard className="card input-field">
-      <input type="text" placeholder="title" />
-      <input type="text" placeholder="body" />
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange("title")}
+        value={title}
+      />
+      <input
+        type="text"
+        placeholder="body"
+        onChange={handleChange("body")}
+        value={body}
+      />
       <div className="file-field input-field">
         <div className="btn #64b5f6 blue darken-1">
           <span>Upload Image</span>
-          <input type="file" />
+          <input type="file" onChange={handleChange("image")} />
         </div>
         <div className="file-path-wrapper">
           <input className="file-path validate" type="text" />
