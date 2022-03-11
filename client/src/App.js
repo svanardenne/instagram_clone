b5d434eefcda3e587c0f65dbc6d97e7312de3b3a
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-03-10 16:51:11 -0800

    Added context api

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,9 @@ export const UserContext = createContext();
 
 const Routing = () => {
   const history = useHistory();
+  const { state, dispatch } = useContext(UserContext);
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
-    const { state, dispatch } = useContext(UserContext);
     if (user) {
       dispatch({ type: "USER", payload: user });
       history.push("/");
